e3c5d5a568ed10187d95c785f2ee0f430bd004c7
leqing92 <leqing92@hotmail.com>
2024-07-16 14:22:37 +0800

    add new chac

--- frontend/src/app/environment.ts
+++ frontend/src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     stripe_publishable_key: 'pk_test_51PWfJg02NgJxI7ypkLEpYrktqIIPBXik7eEnmsZMvs66XxJeAiy8wwLvuEV1LEjgdpg3NmmonosJ1Dwy5pOj14ha00oJE4RoHK',
-    google_recaptcha_site_key: '6LfMOggqAAAAABWM58sADWdYPHofVce8bx1xOYiD'
+    google_recaptcha_site_key: '6LcBe_opAAAAAKiKcboTGEYBfIH3NnSEcVYLd6Xu'
 };
